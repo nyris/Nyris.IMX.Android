8c64e8370bf0de8d206e844151fe9047c37b2239
Sidali Mellouk <s.mellouk@nyris.io>
2018-01-03 14:39:38 +0100

    Added new constructor for ObjectProposal model

--- sdk/src/main/java/models/de/nyris/imx/ObjectProposal.java
+++ sdk/src/main/java/models/de/nyris/imx/ObjectProposal.java
@@ -4,17 +4,43 @@ import android.graphics.RectF;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.io.Serializable;
+
 /**
  * Created by sidali on 30.11.17.
  */
 
-public class ObjectProposal {
+public class ObjectProposal implements Serializable {
     @SerializedName("confidence")
     private float confidence;
 
     @SerializedName("region")
     private RectF region;
-    public ObjectProposal(){}
+
+    @SerializedName("className")
+    private String className;
+
+    public ObjectProposal(){
+
+    }
+
+    public ObjectProposal(float confidence, RectF region, String className){
+        this.confidence = confidence;
+        this.region = region;
+        this.className = className;
+    }
+
+    public void setConfidence(float confidence) {
+        this.confidence = confidence;
+    }
+
+    public void setRegion(RectF region) {
+        this.region = region;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
 
     public float getConfidence() {
         return confidence;
@@ -22,5 +48,9 @@ public class ObjectProposal {
 
     public RectF getRegion() {
         return region;
+    }
+
+    public String getClassName() {
+        return className;
     }
 }
